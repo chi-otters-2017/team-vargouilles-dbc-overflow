69386efda3959c29ba992991b7f61a8885242170
Patrick Hoban & Ashley Yang <pair+patrickjameshoban.yangashley@devbootcamp.com>
2017-01-12 17:20:49 -0600

    changes in answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :question
-  belongs_to :answerer, { class_name: :User}
+  belongs_to :answerer, { class_name: 'User'}
   has_many :votes, :as => :votable
   has_many :comments, as: :commentable
 
